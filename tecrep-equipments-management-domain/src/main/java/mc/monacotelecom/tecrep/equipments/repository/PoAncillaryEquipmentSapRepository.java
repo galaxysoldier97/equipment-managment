94cd9cdf7cf99f03d325bcf6a41f863e80605a27
galaxysoldier97 <github.work.unlock@gmail.com>
2025-07-25 13:16:42 -0500

    Fix PO existence check in AncillaryImportService

--- tecrep-equipments-management-domain/src/main/java/mc/monacotelecom/tecrep/equipments/repository/PoAncillaryEquipmentSapRepository.java
+++ tecrep-equipments-management-domain/src/main/java/mc/monacotelecom/tecrep/equipments/repository/PoAncillaryEquipmentSapRepository.java
@@ -4,4 +4,5 @@ import mc.monacotelecom.tecrep.equipments.entity.PoAncillaryEquipmentSap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PoAncillaryEquipmentSapRepository extends JpaRepository<PoAncillaryEquipmentSap, Long> {
+    boolean existsByPoNo(String poNo);
 }
